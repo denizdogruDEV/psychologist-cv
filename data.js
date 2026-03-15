994f305d63ea7ebc4d194ac3799e8f4102576b00
Deniz <72267288+denizdogruDEV@users.noreply.github.com>
2026-03-15 15:03:47 +0100

    Refactor CV data structure and content

--- data.js
+++ data.js
@@ -7,6 +7,7 @@ const cvData = {
     bio: `Merhaba, burada bugüne kadar almış olduğum eğitimler hakkında bilgiler bulacaksınız. Ben Ege Üniversitesi Psikoloji Bölümü mezunuyum. Yüksek lisansımı da Ege Üniversitesi Psikolojik Danışmanlık ve Rehberlik Bölümünde tamamladım.
 
 Abdülkadir Özbek Psikodrama Dernekleri Federasyonu'nda 948 birim olan psikodrama eğitim sürecini "Psikodramanın Yetişkinlerin Benlik Algısına Ve İdeal Benliğine Etkisi" yeterlilik tezini vererek 2018 yılında bitirdim.`,
+    description: "I'm a paragraph. Click here to add your own text...",
     contact: {
       email: "dominopsikoloji@gmail.com",
       phone: "+90 5331462662",
